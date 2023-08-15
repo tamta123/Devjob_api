04bf04e6d18e76e5c0fb6eb985fcf53b32f07a39
tamta123 <tamtag186@gmail.com>
2023-08-15 12:39:20 +0400

    feat:generate the table from data.json

--- src/config/sql.js
+++ src/config/sql.js
@@ -1,5 +1,6 @@
 import pgk from "pg";
 const { Pool } = pgk; //pool არის ის ქონექშენი რასაც ჩვენი ნოუდი(სერვერი) ქმნის მონაცემთა ბაზასთან
+import fs from "fs/promises"; // Import the 'fs' module to read the JSON file. By using the fs module, you can read external data from files, which is helpful when you want to separate your data from your code or work with data that is dynamically generated or provided from an external source.
 
 const pool = new Pool({
   host: "dpg-cjcsl4ndb61s739aq2ug-a",
@@ -12,9 +13,77 @@ const pool = new Pool({
 //render-ზე არ არის იმის საშუალებ რომ ქრიეთ თეიბლ და ეგეთები ვაკეთოთ და pgadmin-ში არის ამიტო აქვე უნდა დავწეროთ ცხრილის შექმნის ფუნქცია
 
 export const createTable = async () => {
-  return await pool.query(
-    "CREATE TABLE IF NOT EXISTS jobs(id SERIAL PRIMARY KEY, company TEXT, logo_path INT, logo_background_color TEXT, position TEXT, posted_at TEXT,contract TEXT,location TEXT,website TEXT,apply TEXT,description TEXT)"
+  const jsonData = await fs.readFile("jobsData.json", "utf-8");
+  const jobsData = JSON.parse(jsonData); //This function parses a JSON string and returns a JavaScript object.
+  await pool.query(
+    "CREATE TABLE IF NOT EXISTS jobs(" +
+      "id SERIAL PRIMARY KEY, " +
+      "company TEXT, " +
+      "logo TEXT, " +
+      "logo_background_color TEXT, " +
+      "position TEXT, " +
+      "posted_at TEXT, " +
+      "contract TEXT, " +
+      "location TEXT, " +
+      "website TEXT, " +
+      "apply TEXT, " +
+      "description TEXT" +
+      ");"
   );
+
+  // Create "requirements" table
+  await pool.query(
+    "CREATE TABLE IF NOT EXISTS requirements(" +
+      "id SERIAL PRIMARY KEY, " +
+      "job_id INT REFERENCES jobs(id), " +
+      "content TEXT" +
+      ");"
+  );
+
+  // Create "roles" table
+  await pool.query(
+    "CREATE TABLE IF NOT EXISTS roles(" +
+      "id SERIAL PRIMARY KEY, " +
+      "job_id INT REFERENCES jobs(id), " +
+      "content TEXT" +
+      ");"
+  );
+
+  // Populate tables with data
+  for (const jobData of jobsData) {
+    // Insert job data into the "jobs" table and retrieve the inserted job's ID
+    const { rows } = await pool.query(
+      "INSERT INTO jobs(company, logo, logo_background_color, position, posted_at, contract, location, website, apply, description) " +
+        "VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING id;",
+      [
+        jobData.company,
+        jobData.logo,
+        jobData.logoBackground,
+        jobData.position,
+        jobData.postedAt,
+        jobData.contract,
+        jobData.location,
+        jobData.website,
+        jobData.apply,
+        jobData.description,
+      ]
+    );
+
+    // Extract the inserted job's ID from the query result
+    const jobId = rows[0].id;
+
+    // Insert requirements data into the "requirements" table using the job's ID
+    await pool.query(
+      "INSERT INTO requirements(job_id, content) VALUES ($1, $2);",
+      [jobId, jobData.requirements.content]
+    );
+
+    // Insert roles data into the "roles" table using the job's ID
+    await pool.query("INSERT INTO roles(job_id, content) VALUES ($1, $2);", [
+      jobId,
+      jobData.role.content,
+    ]);
+  }
 };
 
 export default pool;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import pool, { createTable } from "./config/sql.js";
+import bodyParser from "body-parser";
 
 const app = express();
 app.use("/images", express.static("public/images"));
@@ -11,6 +12,7 @@ const init = async () => {
     console.log(error);
   }
   function startServer() {
+    app.use(bodyParser.json()); //ბადი პარსერი საშუალებას იძლევა რომ ინფორმაცია იყოს წაკითხული ჯეისონ ფორმატში
     app.get("/api/jobs", async (_, response) => {
       try {
         const resultQuery = await pool.query("SELECT * FROM JOBS"); // ეს აბრუნებს ობიექტის სახით პასუხს
@@ -21,6 +23,11 @@ const init = async () => {
       } // აქ გვინდა რომ წამოიღოს ბაზიდან და გამოგვიტანოს
     }); //პირველი პარამეტრია როუთი(ენდფოინთი) სადაც უნდა გაეშვას ეს რექუსთი და მეორე რა ფუნქცია გეშვას ამ დროს
     // სადაც ვწერთ კოდს რომელიც ამ როუთზე შესვლისას გაეშვება
+
+    app.post("/api/jobs", async (req, res) => {
+      const { company, logo_path } = req.body;
+      //font-ში ვიღაც ავსებს ინფორმაციის ფომრას, მოდის ინფორმაცია ბექის სერვერზე, ბექის სერვერზე ამუშავებს და იმ ინფორმაციის საფუძველზე ქმნის ახალ ჩანაწერს მონაცემთა ბაზაში
+    }); //პოსტი მიგვანიშნებს რომ ახალი ემატება და გეთი  მიგვანიშნებს რომ ინფორმაცია მოგვაქვს
     app.listen(3000);
   }
 };
